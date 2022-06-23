335fcdb81126549379a96a90895a0ac0d0d585a3
Dinesh kumar <64835248+Dinesh305@users.noreply.github.com>
2022-06-23 13:50:32 +0200

    added few steps

--- Ankit_Products_customer.sql
+++ Ankit_Products_customer.sql
@@ -1,16 +1,16 @@
---create table products
---(
---product_id varchar(20) ,
---cost int
---);
---insert into products values ('P1',200),('P2',300),('P3',500),('P4',800);
---drop table if exists customer_budget;
---create table customer_budget
---(
---customer_id int,
---budget int
---);
---insert into customer_budget values (100,400),(200,800),(300,1500);
+create table products
+(
+product_id varchar(20) ,
+cost int
+);
+insert into products values ('P1',200),('P2',300),('P3',500),('P4',800);
+drop table if exists customer_budget;
+create table customer_budget
+(
+customer_id int,
+budget int
+);
+insert into customer_budget values (100,400),(200,800),(300,1500);
 
 
 ;with cte_1 as
