82f301667f779d718acb9ea0e48f6a832c2ddcb1
Your Name <dineshkumarganesn05@outlook.com>
2022-06-23 13:48:45 +0200

    ne code

--- Ankit_Products_customer.sql
+++ Ankit_Products_customer.sql
@@ -1,16 +1,22 @@
-drop table if exists products
-create table ankit_products
-(
-product_id varchar(20) ,
-cost int
-);
-insert into ankit_products values ('P1',200),('P2',300),('P3',500),('P4',800);
-;
-drop table if exists ankit_customer_budget
-create table ankit_customer_budget
-(
-customer_id int,
-budget int);
-insert into ankit_customer_budget values (100,400),(200,800),(300,1500);
------------------------------------------------------------------------------------------
+--create table products
+--(
+--product_id varchar(20) ,
+--cost int
+--);
+--insert into products values ('P1',200),('P2',300),('P3',500),('P4',800);
+--drop table if exists customer_budget;
+--create table customer_budget
+--(
+--customer_id int,
+--budget int
+--);
+--insert into customer_budget values (100,400),(200,800),(300,1500);
 
+
+;with cte_1 as
+(
+select product_id,sum(cost) over (order by product_id) s_cost from products
+)
+select customer_id,budget,STRING_AGG(product_id, ',') agg  from cte_1 p 
+join customer_budget c on c.budget >p.s_cost
+group by budget,customer_id
